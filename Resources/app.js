d02da3df7a205c1ec29f93d7f5a44b86c952b912
Mariusz Lusiak <mariusz@applicake.com>
2011-01-13 20:18:51 +0100

    Adds the basic metronome functionality

--- Resources/app.js
+++ Resources/app.js
@@ -1,55 +1,77 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+// this sets the background color of the master UIView 
+// (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
 // create tab group
 var tabGroup = Titanium.UI.createTabGroup();
 
-
 //
 // create base UI tab and root window
 //
 var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
+    title : 'Metronome Window',
+    backgroundColor : '#fff'
 });
 var tab1 = Titanium.UI.createTab({  
     icon:'KS_nav_views.png',
-    title:'Tab 1',
+    title:'Metronome Tab',
     window:win1
 });
 
 var label1 = Titanium.UI.createLabel({
 	color:'#999',
-	text:'I am Window 1',
+	text:'Here comes the metronome',
 	font:{fontSize:20,fontFamily:'Helvetica Neue'},
 	textAlign:'center',
 	width:'auto'
 });
 
-win1.add(label1);
 
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
+var view = Ti.UI.createView({
+	backgroundColor : "#f00"
 });
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
+win1.add(view);
 
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
+var file = Titanium.Filesystem.getFile(
+	Titanium.Filesystem.resourcesDirectory, "klack.wav"
+);
 
-win2.add(label2);
+var sound = Titanium.Media.createSound({sound:file});
+
+var red = true;
+setInterval(function()
+{
+	view.backgroundColor = red ? "#0f0" : "#f00"
+	sound.play();
+	red = !red
+}, 440);
+
+view.add(label1);
+
+
+
+// //
+// // create controls tab and root window
+// //
+// var win2 = Titanium.UI.createWindow({  
+//     title:'Tab 2',
+//     backgroundColor:'#fff'
+// });
+// var tab2 = Titanium.UI.createTab({  
+//     icon:'KS_nav_ui.png',
+//     title:'Tab 2',
+//     window:win2
+// });
+// 
+// var label2 = Titanium.UI.createLabel({
+// 	color:'#999',
+// 	text:'I am Window 2',
+// 	font:{fontSize:20,fontFamily:'Helvetica Neue'},
+// 	textAlign:'center',
+// 	width:'auto'
+// });
+// 
+// win2.add(label2);
 
 
 
@@ -57,7 +79,7 @@ win2.add(label2);
 //  add tabs
 //
 tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
+// tabGroup.addTab(tab2);  
 
 
 // open tab group
